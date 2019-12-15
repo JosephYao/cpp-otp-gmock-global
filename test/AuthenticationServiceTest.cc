a1da80af2e8a60412799e017b886035f8ad59bf0
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2019-12-15 11:09:19 +0800

    clean up

--- test/AuthenticationServiceTest.cc
+++ test/AuthenticationServiceTest.cc
@@ -8,20 +8,11 @@ using namespace ::testing;
 
 MOCK_GLOBAL_FUNC0(createRsaTokenDao, std::unique_ptr<RsaTokenDao>());
 
-//std::unique_ptr<StubRsaTokenDao> stubRsaTokenDao = std::make_unique<StubRsaTokenDao>();;
-//
-//std::unique_ptr<RsaTokenDao> createRsaTokenDao(){
-//    return std::move(stubRsaTokenDao);
-//}
-
 namespace {
 
     TEST(AuthenticationService, IsValid) {
         AuthenticationService target;
-//        auto stubRsaTokenDao = std::make_unique<StubRsaTokenDao>();
         auto *stubRsaTokenDao = new StubRsaTokenDao();
-//        auto stubRsaTokenDaoPtr = stubRsaTokenDao.get();
-//        StubRsaTokenDao stubRsaTokenDao;
 
         ON_GLOBAL_CALL(createRsaTokenDao, createRsaTokenDao()).WillByDefault(Return(ByMove(std::unique_ptr<RsaTokenDao>(stubRsaTokenDao))));
         ON_CALL(*stubRsaTokenDao, getRandom(_)).WillByDefault(Return("000000"));
