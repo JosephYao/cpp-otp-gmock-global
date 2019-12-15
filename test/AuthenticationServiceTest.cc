22255e52ae165dafa80374056158072ecc5bcda8
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2019-12-15 10:53:55 +0800

    stub global create function with gmock global

--- test/AuthenticationServiceTest.cc
+++ test/AuthenticationServiceTest.cc
@@ -2,22 +2,29 @@
 #include "../main/AuthenticationService.h"
 #include "StubRsaTokenDao.h"
 #include "gmock/gmock.h"
+#include "gmock-global.h"
 
 using namespace ::testing;
 
-std::unique_ptr<StubRsaTokenDao> stubRsaTokenDao = std::make_unique<StubRsaTokenDao>();;
+MOCK_GLOBAL_FUNC0(createRsaTokenDao, std::unique_ptr<RsaTokenDao>());
 
-std::unique_ptr<RsaTokenDao> createRsaTokenDao(){
-    return std::move(stubRsaTokenDao);
-}
+//std::unique_ptr<StubRsaTokenDao> stubRsaTokenDao = std::make_unique<StubRsaTokenDao>();;
+//
+//std::unique_ptr<RsaTokenDao> createRsaTokenDao(){
+//    return std::move(stubRsaTokenDao);
+//}
 
 namespace {
 
     TEST(AuthenticationService, IsValid) {
         AuthenticationService target;
-        auto stubRsaTokenDaoPtr = stubRsaTokenDao.get();
+//        auto stubRsaTokenDao = std::make_unique<StubRsaTokenDao>();
+        auto *stubRsaTokenDao = new StubRsaTokenDao();
+//        auto stubRsaTokenDaoPtr = stubRsaTokenDao.get();
+//        StubRsaTokenDao stubRsaTokenDao;
 
-        ON_CALL(*stubRsaTokenDaoPtr, getRandom(_)).WillByDefault(Return("000000"));
+        ON_GLOBAL_CALL(createRsaTokenDao, createRsaTokenDao()).WillByDefault(Return(ByMove(std::unique_ptr<RsaTokenDao>(stubRsaTokenDao))));
+        ON_CALL(*stubRsaTokenDao, getRandom(_)).WillByDefault(Return("000000"));
 
         bool actual = target.isValid("joey", "91000000");
 
